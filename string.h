f15322705633e4b275f0be3a4390b1c6f51f285e
e7e26e5 <abc@xyz.com>
2023-06-16 16:19:16 +0800

    reformat

--- string.h
+++ string.h
@@ -1,65 +1,64 @@
 #ifndef __MYSTRING__
 #define __MYSTRING__
 
-class String
-{
-public:                                 
-   String(const char* cstr=0);                     
-   String(const String& str);                    
-   String& operator=(const String& str);         
-   ~String();                                    
-   char* get_c_str() const { return m_data; }
+class String {
+public:
+    String(const char *cstr = 0);
+
+    String(const String &str);
+
+    String &operator=(const String &str);
+
+    ~String();
+
+    char *get_c_str() const { return m_data; }
+
 private:
-   char* m_data;
+    char *m_data;
 };
 
 #include <cstring>
 
 inline
-String::String(const char* cstr)
-{
-   if (cstr) {
-      m_data = new char[strlen(cstr)+1];
-      strcpy(m_data, cstr);
-   }
-   else {   
-      m_data = new char[1];
-      *m_data = '\0';
-   }
+String::String(const char *cstr) {
+    if (cstr) {
+        m_data = new char[strlen(cstr) + 1];
+        strcpy(m_data, cstr);
+    } else {
+        m_data = new char[1];
+        *m_data = '\0';
+    }
 }
 
 inline
-String::~String()
-{
-   delete[] m_data;
+String::~String() {
+    delete[] m_data;
 }
 
 inline
-String& String::operator=(const String& str)
-{
-   if (this == &str)
-      return *this;
-
-   delete[] m_data;
-   m_data = new char[ strlen(str.m_data) + 1 ];
-   strcpy(m_data, str.m_data);
-   return *this;
+String &String::operator=(const String &str) {
+    if (this == &str) {
+        return *this;
+    }
+    delete[] m_data;
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
+    return *this;
 }
 
 inline
-String::String(const String& str)
-{
-   m_data = new char[ strlen(str.m_data) + 1 ];
-   strcpy(m_data, str.m_data);
+String::String(const String &str) {
+    m_data = new char[strlen(str.m_data) + 1];
+    strcpy(m_data, str.m_data);
 }
 
 #include <iostream>
+
 using namespace std;
 
-ostream& operator<<(ostream& os, const String& str)
-{
-   os << str.get_c_str();
-   return os;
+ostream &operator<<(ostream &os, const String &str) {
+    os << str.get_c_str();
+    return os;
 }
 
 #endif
